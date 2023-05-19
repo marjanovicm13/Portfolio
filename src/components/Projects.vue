9fe4cdefc21ff28c353943035fe3e4798c6afa40
marjanovicm13 <marjanovicm314@gmail.com>
2023-05-19 20:36:44 +0200

    Added more projects

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -40,6 +40,86 @@
             </div>
         </div>
      </div> 
+
+     <div class="flex-container">
+        <div> 
+            <div class="card flex justify-content-center" id="gallery">
+                <Galleria :value="project3" :responsiveOptions="responsiveOptions" :numVisible="5" containerStyle="max-width: 640px"
+                    :showThumbnails="false" :showIndicators="true">
+                    <template #item="slotProps">
+                        <img :src="require(`@/assets/hangman/${slotProps.item.itemImageSrc}`)" :alt="slotProps.item.alt" style="width: 100%; display: block" />
+                    </template>
+                </Galleria>
+            </div>
+        </div>
+        <div>
+            Simple hangman game made while learning Javascript
+            <br><br>
+            Technologies used: HTML, CSS, Javascript
+            <br><br>
+            <a href="https://github.com/marjanovicm13/Hangman" target="_blank">Github link</a>
+        </div>
+     </div> 
+
+     <div class="flex-container">
+        <div>
+            Image slider made for an assignment 
+            <br><br>
+            Technologies used: HTML, CSS, SCSS, Javascript
+            <br><br>
+            <a href="https://github.com/marjanovicm13/ImageSlider" target="_blank">Github link</a>
+        </div>
+        <div> 
+            <div class="card flex justify-content-center" id="gallery">
+                <Galleria :value="project4" :responsiveOptions="responsiveOptions" :numVisible="5" containerStyle="max-width: 640px"
+                    :showThumbnails="false" :showIndicators="true">
+                    <template #item="slotProps">
+                        <img :src="require(`@/assets/imageslider/${slotProps.item.itemImageSrc}`)" :alt="slotProps.item.alt" style="width: 100%; height:400px; display: block" />
+                    </template>
+                </Galleria>
+            </div>
+        </div>
+     </div> 
+
+     <div class="flex-container">
+        <div> 
+            <div class="card flex justify-content-center" id="gallery">
+                <Galleria :value="project5" :responsiveOptions="responsiveOptions" :numVisible="5" containerStyle="max-width: 640px"
+                    :showThumbnails="false" :showIndicators="true">
+                    <template #item="slotProps">
+                        <img :src="require(`@/assets/HammerAngular/${slotProps.item.itemImageSrc}`)" :alt="slotProps.item.alt" style="width: 100%; display: block" />
+                    </template>
+                </Galleria>
+            </div>
+        </div>
+        <div>
+            CRUD web app made for an internship while learning Angular
+            <br><br>
+            Technologies used: HTML, CSS, Angular, .NET
+            <br><br>
+            <a href="https://github.com/marjanovicm13/HammerProject" target="_blank">Github link</a>
+        </div>
+     </div> 
+
+     <div class="flex-container">
+        <div>
+            CRUD web app made for an internship while learning Blazor
+            <br><br>
+            Technologies used: HTML, CSS, Javascript, Blazor
+            <br><br>
+            <a href="https://github.com/marjanovicm13/HammerProject-in-Blazor" target="_blank">Github link</a>
+        </div>
+        <div> 
+            <div class="card flex justify-content-center" id="gallery">
+                <Galleria :value="project6" :responsiveOptions="responsiveOptions" :numVisible="5" containerStyle="max-width: 640px"
+                    :showThumbnails="false" :showIndicators="true">
+                    <template #item="slotProps">
+                        <img :src="require(`@/assets/HammerBlazor/${slotProps.item.itemImageSrc}`)" :alt="slotProps.item.alt" style="width: 100%; height:400px; display: block" />
+                    </template>
+                </Galleria>
+            </div>
+        </div>
+     </div> 
 </template>
 
 
@@ -52,10 +132,19 @@ import { PhotoService } from '@/service/PhotoService'
 onMounted(() => {
     PhotoService.getImages(1).then((data) => (project1.value = data));
     PhotoService.getImages(2).then((data) => (project2.value = data));
+    PhotoService.getImages(3).then((data) => (project3.value = data));
+    PhotoService.getImages(4).then((data) => (project4.value = data));
+    PhotoService.getImages(5).then((data) => (project5.value = data));
+    PhotoService.getImages(6).then((data) => (project6.value = data));
 });
 
 const project1 = ref();
 const project2 = ref();
+const project3 = ref();
+const project4 = ref();
+const project5 = ref();
+const project6 = ref();
+
 const responsiveOptions = ref([
     {
         breakpoint: '991px',
--- src/service/PhotoService.js
+++ src/service/PhotoService.js
@@ -39,6 +39,112 @@ export const PhotoService = {
             }
         ];
     }
+    else if(project == 3)
+    {
+        return [
+            {
+                itemImageSrc: "main.png",
+                alt: 'Description for Image 1',
+                title: 'Title 1'
+            },
+            {
+                itemImageSrc: "start.png",
+                alt: 'Description for Image 2',
+                title: 'Title 2'
+            },
+            {
+                itemImageSrc: "ingame.png",
+                alt: 'Description for Image 3',
+                title: 'Title 3'
+            },
+            {
+                itemImageSrc: "won.png",
+                alt: 'Description for Image 4',
+                title: 'Title 4'
+            }
+            ,
+            {
+                itemImageSrc: "lost.png",
+                alt: 'Description for Image 5',
+                title: 'Title 5'
+            }
+        ];
+    }
+    else if(project == 4)
+    {
+        return [
+            {
+                itemImageSrc: "main.png",
+                alt: 'Description for Image 1',
+                title: 'Title 1'
+            },
+            {
+                itemImageSrc: "slider.gif",
+                alt: 'Description for Image 2',
+                title: 'Title 2'
+            }
+        ];
+    }
+    else if(project == 5)
+    {
+        return [
+            {
+                itemImageSrc: "main.png",
+                alt: 'Description for Image 1',
+                title: 'Title 1'
+            },
+            {
+                itemImageSrc: "queries.png",
+                alt: 'Description for Image 2',
+                title: 'Title 2'
+            },
+            {
+                itemImageSrc: "login.png",
+                alt: 'Description for Image 2',
+                title: 'Title 2'
+            }
+        ];
+    }
+    else if(project == 6)
+    {
+        return [
+            {
+                itemImageSrc: "main.png",
+                alt: 'Description for Image 1',
+                title: 'Title 1'
+            },
+            {
+                itemImageSrc: "queries.png",
+                alt: 'Description for Image 2',
+                title: 'Title 2'
+            },
+            {
+                itemImageSrc: "login.png",
+                alt: 'Description for Image 2',
+                title: 'Title 2'
+            },
+            {
+                itemImageSrc: "register.png",
+                alt: 'Description for Image 2',
+                title: 'Title 2'
+            },
+            {
+                itemImageSrc: "loggedinmain.png",
+                alt: 'Description for Image 2',
+                title: 'Title 2'
+            },
+            {
+                itemImageSrc: "update.png",
+                alt: 'Description for Image 2',
+                title: 'Title 2'
+            },
+            {
+                itemImageSrc: "choosedepartment.png",
+                alt: 'Description for Image 2',
+                title: 'Title 2'
+            }
+        ];
+    }
 },
 
     getImages(project) {
